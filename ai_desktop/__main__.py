62e13499fdfa32d3db7f044127091c780a83aae8
setfireonSdom <1395272144@qq.com>
2026-08-12 23:17:34 +0800

    fix: handle multiprocessing subprocess entry

--- ai_desktop/__main__.py
+++ ai_desktop/__main__.py
@@ -1,5 +1,8 @@
 """Support for `python -m ai_desktop` entry point"""
+from multiprocessing import freeze_support
+
 from ai_desktop.main import main
 
 if __name__ == "__main__":
+    freeze_support()
     main()
--- ai_desktop/main.py
+++ ai_desktop/main.py
@@ -882,4 +882,7 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    from multiprocessing import freeze_support
+
+    freeze_support()
     main()
